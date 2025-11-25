a7110696a00ad6b5c1b6fb5cc2ca3ae22218e1c3
Горин Пётр <peter14102005@gmail.com>
2025-11-25 16:15:05 +0300

    ы

---
+++ hexlet_django_blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(
+        request,
+        "index.html",
+        context={
+            "who": "Peter",
+        },
+    )
+
+def about(request):
+    return render(request, "about.html")
